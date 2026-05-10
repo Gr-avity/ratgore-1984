f66b671bdfadb31d25b95cc14261f817031409ac
Ko-mp-otik <gravityrassist@gmail.com>
2026-05-10 20:26:20 +0400

    ship repair device

--- Content.Server/_Crescent/Shipyard/ShipyardPatch.cs
+++ Content.Server/_Crescent/Shipyard/ShipyardPatch.cs
@@ -171,7 +171,10 @@ public sealed partial class ShipyardSystem : SharedShipyardSystem
             shuttle = null;
             return false;
         }
-
+        // rat-change start
+        var ev = new ShipBoughtEvent();
+        RaiseLocalEvent(shuttleGrid.Value, ev);
+        // rat-change end
         _sawmill.Info($"Shuttle {shuttlePath} was purchased at {ToPrettyString((EntityUid) stationUid)} for {price:f2}");
         //can do TryFTLDock later instead if we need to keep the shipyard map paused
         _shuttle.TryFTLDock( shuttleGrid.Value, shuttle, targetGrid.Value);
